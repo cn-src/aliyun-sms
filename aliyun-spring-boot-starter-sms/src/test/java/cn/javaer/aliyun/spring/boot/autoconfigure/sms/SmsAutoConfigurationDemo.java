9092c0bcec4877fb65051c91fba4dba12221d083
cn-src <public@javaer.cn>
2018-02-27 11:35:16 +0800

    添加测试样例

---
+++ aliyun-spring-boot-starter-sms/src/test/java/cn/javaer/aliyun/spring/boot/autoconfigure/sms/SmsAutoConfigurationDemo.java
@@ -0,0 +1,25 @@
+package cn.javaer.aliyun.spring.boot.autoconfigure.sms;
+
+import cn.javaer.aliyun.sms.SmsClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author zhangpeng
+ */
+@SpringBootTest(classes = SmsAutoConfiguration.class)
+@RunWith(SpringRunner.class)
+public class SmsAutoConfigurationDemo {
+
+    @Autowired
+    private SmsClient smsClient;
+
+    @Test
+    public void sendVerificationCode() {
+        final String phoneNumber = System.getenv("aliyun.sms.authentication.phoneNumber");
+        this.smsClient.sendVerificationCode("authentication", phoneNumber);
+    }
+}
